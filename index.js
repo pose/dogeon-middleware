99939fea8e83aa1bc99fef259d546b9925c68055
Alberto Pose <albertopose@gmail.com>
2014-06-06 16:14:37 +0200

    validating req.body

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ module.exports = function () {
   return function (req, res, next) {
     var value;
 
-    try {
-      value = DSON.parse(req.body.toString('utf8'));
-    } catch (e) {
-      return next(e);
+    if (req.body && req.body.toString) {
+      try {
+        value = DSON.parse(req.body.toString('utf8'));
+      } catch (e) {
+        return next(e);
+      }
+    } else {
+      return next();
     }
 
     req.body = value;
